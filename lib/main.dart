89f07ac5923203abd9a1f78b72bc6be81ac6a963
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2021-12-07 20:08:36 -0300

    Adicionado classe de página inicial

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,23 @@ class FlutterContadorApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Contador',
+      home: FCHomePage(),
+    );
+  }
+}
+
+class FCHomePage extends StatefulWidget {
+  const FCHomePage({Key? key}) : super(key: key);
+
+  @override
+  _FCHomePageState createState() => _FCHomePageState();
+}
+
+class _FCHomePageState extends State<FCHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(),
     );
   }
 }
